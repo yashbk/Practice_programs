118a35dc2956259684c9fa9567700c75183825ec
dev1 <dev1@gmail.com>
2022-06-29 18:48:00 +0530

    Sorting algorithms and own memcpy function

---
+++ Status_29_06_2022/insertion_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void insertion_sort(int *arr,int len)
+{
+    int key = 0,j=0;
+    for(int i=1;i<len;i++)
+    {
+        key = arr[i];
+        for(j=i-1;j>=0 && arr[j] > key ;j--)
+        {
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = key;
+    }
+}
+
+void print(int *arr,int len)
+{
+    for(int i=0;i<len;i++)
+    {
+        printf("%d ",*(arr+i));
+    }
+}
+
+int main()
+{
+    int arr[] = {5,4,3,7,1,6,3,2};
+    int len = sizeof(arr)/sizeof(int);
+    printf("Before sorting: ");
+    print(arr,len);
+    insertion_sort(arr,len);
+    printf("\nAfter sorting: ");
+    print(arr,len);
+}
